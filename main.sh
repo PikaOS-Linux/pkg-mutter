7fba177fdfec79c4f26934826517e7be825ff906
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-06-26 19:56:56 +0300

    initial push

--- main.sh
+++ main.sh
@@ -1,16 +1,16 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-git clone mutter-vrr https://gitlab.gnome.org/GNOME/mutter -b 44.2
-cd ./mutter-vrr
+git clone https://gitlab.gnome.org/GNOME/mutter -b 44.2
+cp -rvf ./debian ./mutter
+cd ./mutter
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p mutter-vrr_44.2
-cp -rvf ./debian ./mutter-vrr
-dpkg-buildpackage
+LOGNAME=root dh_make --createorig -y -l -p mutter_44.2 || true
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
