bb43a3bd1d2f704c4ae9811bdc744162eb3f07fc
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-03 19:52:15 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,12 +4,13 @@ DEBIAN_FRONTEND=noninteractive
 git clone https://gitlab.gnome.org/GNOME/mutter -b 45.0
 cp -rvf ./debian ./mutter
 cd ./mutter
-for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || echo "Applying Patch $i Failed!"; done
+for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || echo "Applying Patch $i Failed!" && exit 2; done
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
+LOGNAME=root dh_make --createorig -y -l -p mutter_45.0 || true
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
